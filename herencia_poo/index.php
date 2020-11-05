a064c16e0ef0df2a467a55eafac92c9261912ff4
ismaelsuarezp <ismaelsuarezp@gmail.com>
2020-11-05 18:46:26 -0500

    Agregamos ejemplo con propiedades y metodos de clase (static).

--- herencia_poo/index.php
+++ herencia_poo/index.php
@@ -3,18 +3,26 @@
 require_once("modelo/Aprendiz.php");
 require_once("modelo/Instructor.php");
 require_once("modelo/Coordinador.php");
+require_once("modelo/Administrativo.php");
 
-$aprendiz = new Aprendiz();
-$instructor = new Instructor();
+//$aprendiz = new Aprendiz();
+//$instructor = new Instructor();
 $coordinador = new Coordinador();
 $coordinador->setSalario(30000);
+$coordinador2 = new Coordinador();
+$coordinador2->setSalario(222222);
 
-echo "Obj aprendiz: ";
+/*echo "Obj aprendiz: ";
 $aprendiz->ingresar();
 echo "Obj instructor: ";
-$instructor->ingresar();
+$instructor->ingresar();*/
+
+Administrativo::setPropiedadClase(4444444);
 echo "Obj coordinador: ";
 $coordinador->ingresar();
 $coordinador->despedir();
+echo "Obj coordinador2: ";
+$coordinador2->ingresar();
+$coordinador2->despedir();
 
 echo "Todo va bién...<br/>";
--- herencia_poo/modelo/Administrativo.php
+++ herencia_poo/modelo/Administrativo.php
@@ -3,9 +3,11 @@ require_once("Usuario.php");
 
 abstract class Administrativo implements Usuario {
 
+    protected static $propiedadClase = 77777777;
     protected $salario;
 
     public function ingresar(){
+        echo "Valor prop. clase: ". self::$propiedadClase . "<br/>";
         echo "Ingresamos desde administrativo...<br/>";
     }
 
@@ -17,6 +19,12 @@ abstract class Administrativo implements Usuario {
 
     public function setSalario($salario){
         $this->salario = $salario;
+    }
+
+    public static function setPropiedadClase($valor){
+        //Administrativo::$propiedadClase;
+        //self::$propiedadClase;
+        self::$propiedadClase = $valor;
     }
 
 }
